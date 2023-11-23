cc0c9ff1c8b5a02ebbed3904c2461f11956bfa09
ip522 <ioannis.pitoskas22@imperial.ac.uk>
2023-11-23 07:29:18 +0200

    Added utils.py and implemented a few util functions for dataloading

---
+++ src/utils.py
@@ -0,0 +1,99 @@
+import torch
+from torch.utils.data import Subset
+from sklearn.model_selection import train_test_split
+
+
+
+
+def class_balanced_random_split(idx2label, seed=None, test_ratio_per_class=0.15):
+    """
+    Class-balanced dataset split into train and test partitions.
+    
+    Args:
+        idx2label (list): List of labels in the order they appear in the dataset
+        seed (int, optional): Random seed (Default: None)
+        test_ratio_per_class (float, optional): Percentage of test samples per class (Default: 0.15)
+
+    Returns:
+        (tuple):
+            * train_indices (list): list of the indices of the train samples
+            * test_indices (list): list of the indices of the test samples
+    """
+
+    class_indices = {}
+    for idx, label in enumerate(idx2label):
+        if label not in class_indices:
+            class_indices[label] = []
+        class_indices[label].append(idx)
+
+
+    train_indices = []
+    test_indices = []
+    for label, indices in class_indices.items():
+        if len(indices) > 1:
+            train_idx, test_idx = train_test_split(indices, test_size=test_ratio_per_class, random_state=seed)
+        else:
+            train_idx, test_idx = indices.copy(), []
+        train_indices.extend(train_idx)
+        test_indices.extend(test_idx)
+    
+    return train_indices, test_indices
+
+
+
+def get_positive_pairs_indices(pair_ad_dataset, pair_labels):
+    
+    # Get indices of positive pairs
+    positive_pairs_indices = ((pair_labels==1).nonzero(as_tuple=True)[0])
+    
+    # Condition to discard pairs with identical items
+    non_identical_pairs_condition = (positive_pairs_indices // len(pair_ad_dataset.ad_dataset)) != (positive_pairs_indices % len(pair_ad_dataset.ad_dataset))
+    
+    return positive_pairs_indices[non_identical_pairs_condition]
+
+
+
+def get_negative_pairs_indices(pair_ad_dataset, pair_labels):
+    
+    # Get indices of negative pairs
+    negative_pairs_indices = ((pair_labels==0).nonzero(as_tuple=True)[0])
+    
+    return negative_pairs_indices
+
+
+
+def sample_pair_dataset(pair_ad_dataset, n_pairs_positive=1000, n_pairs_negative=1000):
+    
+    
+    pair_labels = torch.tensor([pair_ad_dataset.get_label_by_idx(idx) for idx in range(len(pair_ad_dataset))])
+    
+    # Get indices of positive and negative pairs
+    positive_pairs_indices = get_positive_pairs_indices(pair_ad_dataset, pair_labels)
+    negative_pairs_indices = get_negative_pairs_indices(pair_ad_dataset, pair_labels)
+
+    
+    # How many pairs from each class
+    n_pairs_positive = min(n_pairs_positive, len(positive_pairs_indices))
+    n_pairs_negative = min(n_pairs_negative, len(negative_pairs_indices))
+
+
+    # Initiate the sampling
+    positive_random_samples = torch.randint(0, positive_pairs_indices.size(0), (n_pairs_positive,))
+    negative_random_samples = torch.randint(0, negative_pairs_indices.size(0), (n_pairs_negative,))
+
+
+    # Apply the sampling
+    positive_pairs_indices_sampled = positive_pairs_indices[positive_random_samples].tolist()
+    negative_pairs_indices_sampled = negative_pairs_indices[negative_random_samples].tolist()
+
+    
+    # Concatenate positive and negative indices for new Subset dataset (balanced undersampled dataset)
+    pairs_indices_sampled = positive_pairs_indices_sampled + negative_pairs_indices_sampled
+
+
+    
+    pair_ad_dataset_sampled = Subset(pair_ad_dataset, pairs_indices_sampled)
+    
+    return pair_ad_dataset_sampled
+
+
