82a34fab2e9a52995d626ef91a0c7c7bed99b616
ip522 <ioannis.pitoskas22@imperial.ac.uk>
2023-11-25 03:59:05 +0200

    Added many ops

--- src/main.py
+++ src/main.py
@@ -17,6 +17,10 @@ from transformers import AutoProcessor, CLIPModel
 from torchvision import transforms
 import torch.optim as optim
 from tqdm import tqdm
+import warnings
+import os
+import datetime
+
 
 
 
@@ -28,6 +32,9 @@ from dataloaders import *
 
 
 
+warnings.filterwarnings("ignore")
+
+
 
 if __name__ == '__main__':
 
@@ -37,13 +44,28 @@ if __name__ == '__main__':
     # Set seeds for reproducibility
     random.seed(args.seed)
     np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed(args.seed)
+
+    # torch.manual_seed(args.seed)
+    # torch.cuda.manual_seed(args.seed)
+
     # torch.backends.cudnn.deterministic = True
     # torch.backends.cudnn.benchmark = False
 
 
 
+
+    if args.inference and args.load_model_id is None:
+        raise ValueError("Should load model for inference mode.")
+    
+
+    if 'clip' in args.pretrained_model_name:
+        args.model_type = 'clip'
+    else:
+        raise NotImplementedError(f'Unsupported model_type for {args.pretrained_model_name}')
+
+
+
+    # Dirs
     working_dir = current_dir.parent
     data_dir = working_dir.joinpath('data', 'dataset')
 
@@ -51,6 +73,26 @@ if __name__ == '__main__':
     thumbnail_data_dir = data_dir.joinpath('images')
 
 
+
+    model_dir_prefix = "Model_"
+    experiments_dir = working_dir/'experiments'/args.pretrained_model_name.replace('/','_')
+    # Create directories for logs
+    if os.path.exists(experiments_dir):
+        model_dirs = [model_dir if os.path.isdir(os.path.join(experiments_dir, model_dir)) else None for model_dir in os.listdir(experiments_dir)]
+        model_dirs = list(filter(None, model_dirs))
+        ids = [int(dd.replace(model_dir_prefix,"")) if (model_dir_prefix) in dd and dd.replace(model_dir_prefix,"").isnumeric() else None for dd in model_dirs]
+        ids = list(filter(None, ids))
+        new_id = str(max(ids) + 1) if ids else "1"
+    else:
+        experiments_dir.mkdir(parents=True)
+        new_id = "1"
+
+    new_model_dir = experiments_dir.joinpath(model_dir_prefix + new_id)
+    new_model_dir.mkdir()
+    exit()
+
+
+    # Logging
     [logging.root.removeHandler(handler) for handler in logging.root.handlers[:]]
     # info logger for saving command line outputs during training
     logging.basicConfig(level=logging.INFO, format='%(message)s',
@@ -58,8 +100,13 @@ if __name__ == '__main__':
                    ])
         #  handlers=[logging.FileHandler(os.path.join(new_model_dir, f'trainlogs_{new_id}.log'))
         #            ])
+    
+    logging.info(f'{model_dir_prefix}ID: {new_id}\n')
+    logging.info(datetime.datetime.now())
 
 
+
+    # Device
     device = torch.device('cuda:0' if not args.no_cuda and torch.cuda.is_available() else 'cpu')
     args.cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -74,6 +121,8 @@ if __name__ == '__main__':
 
 
 
+
+
     logging.info("\nArguments:")
     logging.info('\n'.join(f'- {k}: {v}' for k, v in vars(args).items()))
     logging.info('\n'.join(f'args.{k}={v}' for k, v in vars(args).items()))
@@ -82,7 +131,6 @@ if __name__ == '__main__':
 
 
     logging.info("\n\nLoading Dataset...")
-
     pair_train_loader, pair_test_loader = get_pair_dataloaders_combined(args, text_data_filepath, thumbnail_data_dir)
     logging.info(f'Train: {len(pair_train_loader.dataset)} - Test: {len(pair_test_loader.dataset)}')
     
@@ -104,11 +152,9 @@ if __name__ == '__main__':
     # Loss
     loss_fn = ContrastiveLoss(margin=args.margin)
 
-
     # Optimizer
     param_groups = get_param_groups_for_finetuning(model, args.model_type)
     optimizer = optim.AdamW(param_groups, lr=args.lr, betas=(args.beta1, args.beta2), weight_decay=args.weight_decay)
-
 
      # Trainable parameters
     trainable_params = sum(p.numel() for p in optimizer.param_groups[0]['params'] if p.requires_grad)
